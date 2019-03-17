0fb4471659f64d2055357e22776e91922ccdd7d9
subpal26 <subpal26@in.ibm.com>
2019-03-17 19:18:07 +0530

    Added Project

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+This project contains three different microservices demo implementations
+1. Zuul Gateway
+2. Eureka Server
+3. Config Server
---
+++ zuul-gateway/src/main/java/com/stackroute/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.stackroute.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
